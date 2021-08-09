29abff1dd318040099849a806848016c93574c26
tarikgul <tarik@parity.io>
2021-08-09 18:16:54 -0400

    config types

--- src/config.ts
+++ src/config.ts
@@ -1,171 +1,173 @@
-export const RPC_CHAIN_CONSTS = {
-    author: {
-        hasKey: {
-            development: {},
-        },
-        hasSessionKeys: {
-            development: {},
-        },
-        insertKey: {
-            development: {},
-        },
-        pendingExtrinsics: {
-            development: {},
-        },
-        removeExtrinsic: {
-            development: {},
-        },
-        rotateKeys: {
-            development: {},
-        },
-        submitAndWatchExtrinsic: {
-            development: {},
-        },
-        submitExtrinsic: {
-            development: {},
-        }
-    },
-    chain: {
-        getBlock: {
-            development: {},
-        },
-        getBlockHash: {
-            development: {},
-        },
-        getFinalizedHead: {
-            development: {},
-        },
-        getHeader: {
-            development: {},
-        },
-        subscribeAllHeads: {
-            development: {},
-        },
-        subscribeFinalizedHeads: {
-            development: {},
-        },
-        subscribeNewHeads: {
-            development: {},
-        }
-    },
-    offchain: {
-        localStorageGet: {
-            development: {},
-        },
-        localStorageSet: {
-            development: {},
-        }
-    },
-    state: {
-        call: {
-            development: {},
-        },
-        getChildKeys: {
-            development: {},
-        },
-        getChildReadProof: {
-            development: {},
-        },
-        getChildStorage: {
-            development: {},
-        },
-        getChildStorageHash: {
-            development: {},
-        },
-        getChildStorageSize: {
-            development: {},
-        },
-        getKeys: {
-            development: {},
-        },
-        getKeysPaged: {
-            development: {},
-        },
-        getMetadata: {
-            development: {},
-        },
-        getPairs: {
-            development: {},
-        },
-        getReadProof: {
-            development: {},
-        },
-        getRuntimeVersion: {
-            development: {},
-        },
-        getStorage: {
-            development: {},
-        }, 
-        getStorageAt: {
-            development: {},
-        },
-        subscribeRuntimeVersion: {
-            development: {},
-        },
-        subscribeStorage: {
-            development: {},
-        },
-        traceBlock: {
-            development: {},
-        }
-    },
-    system: {
-        accountNextIndex: {
-            development: {},
-        },
-        addLogFilter: {
-            development: {},
-        },
-        addReservedPeer: {
-            development: {},
-        },
-        chain: {
-            development: {},
-        },
-        chainType: {
-            development: {},
-        },
-        dryRun: {
-            development: {},
-        },
-        health: {
-            development: {},
-        },
-        localListenAddresses: {
-            development: {},
-        },
-        localPeerId: {
-            development: {},
-        },
-        name: {
-            development: {},
-        },
-        networkState: {
-            development: {},
-        },
-        nodeRoles: {
-            development: {},
-        },
-        peers: {
-            development: {},
-        },
-        properties: {
-            development: {},
-        },
-        removeReservedPeer: {
-            development: {},
-        },
-        reservedPeers: {
-            development: {},
-        },
-        resetLogFilter: {
-            development: {},
-        },
-        syncState: {
-            development: {},
-        },
-        version: {
-            development: {},
-        }
-    },
+import { RpcConsts } from './types/config';
+
+export const RPC_CHAIN_CONSTS: RpcConsts = {
+	author: {
+		hasKey: {
+			development: {},
+		},
+		hasSessionKeys: {
+			development: {},
+		},
+		insertKey: {
+			development: {},
+		},
+		pendingExtrinsics: {
+			development: {},
+		},
+		removeExtrinsic: {
+			development: {},
+		},
+		rotateKeys: {
+			development: {},
+		},
+		submitAndWatchExtrinsic: {
+			development: {},
+		},
+		submitExtrinsic: {
+			development: {},
+		},
+	},
+	chain: {
+		getBlock: {
+			development: {},
+		},
+		getBlockHash: {
+			development: {},
+		},
+		getFinalizedHead: {
+			development: {},
+		},
+		getHeader: {
+			development: {},
+		},
+		subscribeAllHeads: {
+			development: {},
+		},
+		subscribeFinalizedHeads: {
+			development: {},
+		},
+		subscribeNewHeads: {
+			development: {},
+		},
+	},
+	offChain: {
+		localStorageGet: {
+			development: {},
+		},
+		localStorageSet: {
+			development: {},
+		},
+	},
+	state: {
+		call: {
+			development: {},
+		},
+		getChildKeys: {
+			development: {},
+		},
+		getChildReadProof: {
+			development: {},
+		},
+		getChildStorage: {
+			development: {},
+		},
+		getChildStorageHash: {
+			development: {},
+		},
+		getChildStorageSize: {
+			development: {},
+		},
+		getKeys: {
+			development: {},
+		},
+		getKeysPaged: {
+			development: {},
+		},
+		getMetadata: {
+			development: {},
+		},
+		getPairs: {
+			development: {},
+		},
+		getReadProof: {
+			development: {},
+		},
+		getRuntimeVersion: {
+			development: {},
+		},
+		getStorage: {
+			development: {},
+		},
+		getStorageAt: {
+			development: {},
+		},
+		subscribeRuntimeVersion: {
+			development: {},
+		},
+		subscribeStorage: {
+			development: {},
+		},
+		traceBlock: {
+			development: {},
+		},
+	},
+	system: {
+		accountNextIndex: {
+			development: {},
+		},
+		addLogFilter: {
+			development: {},
+		},
+		addReservedPeer: {
+			development: {},
+		},
+		chain: {
+			development: {},
+		},
+		chainType: {
+			development: {},
+		},
+		dryRun: {
+			development: {},
+		},
+		health: {
+			development: {},
+		},
+		localListenAddresses: {
+			development: {},
+		},
+		localPeerId: {
+			development: {},
+		},
+		name: {
+			development: {},
+		},
+		networkState: {
+			development: {},
+		},
+		nodeRoles: {
+			development: {},
+		},
+		peers: {
+			development: {},
+		},
+		properties: {
+			development: {},
+		},
+		removeReservedPeer: {
+			development: {},
+		},
+		reservedPeers: {
+			development: {},
+		},
+		resetLogFilter: {
+			development: {},
+		},
+		syncState: {
+			development: {},
+		},
+		version: {
+			development: {},
+		},
+	},
 };
