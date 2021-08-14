ffb10c7b4aa41324301e4501cad439987ef293c5
tarikgul <tarik@parity.io>
2021-08-14 15:39:59 -0400

    add responses export

---
+++ src/responses.ts
@@ -0,0 +1,12 @@
+export const SubstrateDevHealthRes = {
+    peers: 0,
+    isSyncing: false,
+    shouldHavePeers: false,
+};
+
+export const SubstrateDevLocalAddresses = [
+    '/ip4/127.0.0.1/tcp/30333/p2p/12D3KooWSiRgzMbqZz2pA8JGd8t3SW2Eu6bVeqiETowwwrLuLznT',
+    '/ip4/192.168.86.249/tcp/30333/p2p/12D3KooWSiRgzMbqZz2pA8JGd8t3SW2Eu6bVeqiETowwwrLuLznT',
+    '/ip4/192.168.2.1/tcp/30333/p2p/12D3KooWSiRgzMbqZz2pA8JGd8t3SW2Eu6bVeqiETowwwrLuLznT',
+    '/ip6/::1/tcp/30333/p2p/12D3KooWSiRgzMbqZz2pA8JGd8t3SW2Eu6bVeqiETowwwrLuLznT',
+];
