03672b8e0724678e06876b72d442096d70b6a564
tarikgul <tarik@parity.io>
2021-09-07 16:54:22 -0400

    remove rpcToLocalNode

--- src/util/helpers.ts
+++ src/util/helpers.ts
@@ -2,41 +2,6 @@ import { KeyringPair } from '@polkadot/keyring/types';
 import { Metadata } from '@polkadot/types';
 import { EXTRINSIC_VERSION } from '@polkadot/types/extrinsic/v4/Extrinsic';
 import { createMetadata, OptionsWithMeta } from '@substrate/txwrapper-polkadot';
-import fetch from 'node-fetch';
-
-/**
- * Send a JSONRPC request to the node at http://localhost:9933.
- *
- * @param method - The JSONRPC request method.
- * @param params - The JSONRPC request params.
- */
-export function rpcToLocalNode(
-	method: string,
-	params: (string | number)[] = []
-): Promise<any> {
-	return fetch('http://localhost:9933', {
-		body: JSON.stringify({
-			id: 1,
-			jsonrpc: '2.0',
-			method,
-			params,
-		}),
-		headers: {
-			'Content-Type': 'application/json',
-		},
-		method: 'POST',
-	})
-		.then((response) => response.json())
-		.then(({ error, result }) => {
-			if (error) {
-				throw new Error(
-					`${error.code} ${error.message}: ${JSON.stringify(error.data)}`
-				);
-			}
-
-			return result;
-		});
-}
 
 /**
  * Signing function. Implement this on the OFFLINE signing device.
