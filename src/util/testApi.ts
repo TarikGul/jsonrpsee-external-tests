5ee83597c7d46d6338e04b09d25fc60510991a8b
tarikgul <tarik@parity.io>
2021-09-07 16:53:22 -0400

    adjust the testApi for constructing transactions, param api

--- src/util/testApi.ts
+++ src/util/testApi.ts
@@ -151,7 +151,7 @@ export const runTest = async (
 	 * If thats the case we run the tx and pass it into the api call.
 	 */
 	if (chainSpecMethods.callConstructTx) {
-		tx = await chainSpecMethods.callConstructTx();
+		tx = await chainSpecMethods.callConstructTx(api);
 	}
 
 	/**
